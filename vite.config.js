5b2933fefcf445a450f1e4d2f7395313c84a4da4
Priyansh Sahu <priyansh@Priyanshs-MacBook-Air.local>
2026-05-17 00:25:32 +0530

    fixed socket dependencies issue

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['some-package']
-  }
-});
+  plugins: [react()],
+})
